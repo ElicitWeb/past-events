0df835dda74190783efc6d7ff875c2c6b9d0a44b
ElicitWeb <144059043+ElicitWeb@users.noreply.github.com>
2023-09-07 18:48:30 +0530

    Update Body.jsx

--- Body.jsx
+++ Body.jsx
@@ -1,8 +1,13 @@
 import "@fontsource/readex-pro";
 import React from "react";
 import './body.css';
-import img from './pfp.jpg';
-// import './another.css';
+import decrypt from './decrypt.jpg';
+import defi from './Defi.jpg';
+import gamers from './Gamers.jpg';
+import eminent from './eminent.jpg';
+import futsal from './futsal.jpg';
+import hacks from './hacks.jpg';
+import hunt from './hunt.jpg';
 
 const Body = () => {
   return(
@@ -15,7 +20,7 @@ const Body = () => {
                 <div className="ti2"></div>
                 <div className="ti3"> </div>
               </div>
-              <div className="ev5" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve5">event 5</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
+              <div className="ev5"><img src={eminent} className="image"/><div className="text" id="eve5">Speaker Program</div><div className="mat">The ACM India Eminent speaker program(ESP) provides local ACM chapters with direct access to top technology leaders. During Elicit'22 MUJ ACM had the privilege of inviting Rahul Johari, a renowned professor at Guru Gobind Indraprastha University, Head of the Software Development Cell as well as head of SWINGER, he shared his profound insights and expertise on the field of Blockchains with our students and gave them a headstart in this emerging field.</div></div>
             </div>
           </div>
           <div className="four-ti-two-ev">
@@ -24,10 +29,10 @@ const Body = () => {
                 <div className="ti4"> </div>
                 <div className="ti5"> </div>
               </div>
-              <div className="ev6" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve6">event 6</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
+              <div className="ev6"><img src={defi} className="image"/><div className="text" id="eve6">DeFi Hacks</div><div className="mat">DeFi-Hacks represented a fusion of blockchain and web technologies in a hackathon curated for the students. This event encouraged participants to test their practical knowledge and demonstrate their Web 3.0 prototypes through real-world challenges, offering a whopping cash prize.</div></div>
             </div>
             <div className="two-ti-ev2">
-              <div className="ev7" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve7">event 7</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
+              <div className="ev7"><img src={hunt} className="image"/><div className="text" id="eve7">Treasure Hunt</div><div className="mat"></div></div>
               <div className="two-ti2">
                 <div className="ti6"> </div>
                 <div className="ti7"> </div>
@@ -37,17 +42,17 @@ const Body = () => {
         </div>
         <div className="mob2">
           <div className="ev-ti">
-            <div className="ev1" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve1">event 1</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
+            <div className="ev1"><img src={decrypt} className="image"/><div className="text" id="eve1">Decrypt 2.0</div><div className="mat">Decrypt was a big online cryptic hunt that lasted a whole day in which participants had to implement their unique computer techniques to uncover hidden clues scattered throughout the internet. It was all about cybersecurity, like being a digital detective racing to find these secret hints and win the prize money.</div></div>
             <div className="ti8"> </div>
           </div>
           <div className="three-ev-two-ti">
             <div className="ev-ev">
-              <div className="ev2" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve2">event 2</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
-              <div className="ev3" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve3">event 3</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
+              <div className="ev2"><img src={gamers} className="image"/><div className="text" id="eve2">Gamers Assylum</div><div className="mat">The Gamers Asylum event spanned 13 hours and featured three exciting games: FIFA22, Call of Duty, and F1. Throughout the event, gamers faced off in exciting matches, showing off their gaming skills and clever tactics to outplay their opponents,all in pursuit of a highly sought-after reward.</div></div>
+              <div className="ev3"><img src={hacks} className="image"/><div className="text" id="eve3">Hacks 7.0</div><div className="mat">Hacks 7.0 engaged the participants in a demanding 36-hour challenge, racing against time to build and improve their project. The event provided a launching base for many budding tech enthusiasts to go forward with their skills in design, development and pitching.</div></div>
             </div>
             <div className="two-ti-ev">  
               <div className="ti9"> </div>
-              <div className="ev4" style={{backgroundImage: `url(${img})`}}><div className="text" id="eve4">event 4</div><div className="mat">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequis.</div></div>
+              <div className="ev4"><img src={futsal} className="image"/><div className="text" id="eve4">Futsal</div><div className="mat">As we eagerly counted down to the kickoff of World Cup 2022, MUJ ACM took the initiative to host a thrilling game of Futsal during Elicit'22! This event was a brilliant display of our Technocultural spirit, emphasizing the invaluable journey of nurturing our cherished members' well-rounded growth. It was a thrill from start to finish!</div></div>
               <div className="ti10"> </div>
             </div>
           </div>
